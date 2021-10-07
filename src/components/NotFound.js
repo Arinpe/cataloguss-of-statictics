abcbfe3c293ef108588644db98326bdc95487137
Arinpe <dehtolah@gmail.com>
2021-10-07 12:49:39 +0100

    Add the Navlink file into the components folder

---
+++ src/components/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/NotFound.scss';
+
+const NotFound = () => (
+  <div className="notfound">
+    <span><i className="fa fa-chain-broken fa-5" /></span>
+    <h2>
+      <span> 4</span>
+      <span>0</span>
+      <span>4</span>
+    </h2>
+    <h1>Page Not Found</h1>
+  </div>
+);
+
+export default NotFound;
