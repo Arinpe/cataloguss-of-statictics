1bc191da56b8c0f420c209253f7ee939d9d20fee
Arinpe <dehtolah@gmail.com>
2021-10-08 12:02:02 +0100

    Fixed linters error

--- src/_test_/components/Navbar.test.js
+++ src/_test_/components/Navbar.test.js
@@ -1,5 +1,5 @@
 import Navbar from '../../components/Navbar';
-import shallowWrapper from '../testSetup';
+import shallowWrapper from './testSetup';
 
 describe('Navigation bar', () => {
   const nav = shallowWrapper(Navbar);
