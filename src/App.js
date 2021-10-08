68e10cdd461bfd0dfb1c07a9bccdfb5343e63078
Arinpe <dehtolah@gmail.com>
2021-10-08 13:01:21 +0100

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import HomePage from './containers/homepage';
+import MoviePage from './containers/Video';
+import NotFound from './components/NotFound';
+import NavBar from './components/Navbar';
+
+const App = () => (
+  <Router>
+    <div className="App max-width">
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/:movie" component={MoviePage} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
-  );
-}
+  </Router>
+);
 
 export default App;
